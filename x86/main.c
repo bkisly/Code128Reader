00a1f9dd532046a5171606342556ce68d72ccb7f
bkisly <b.kisly@gmail.com>
2022-06-07 19:06:35 +0200

    Added reading command line arguments

--- x86/main.c
+++ x86/main.c
@@ -9,11 +9,12 @@ extern uint8_t getNarrowestBar(unsigned char *beginAddress, unsigned char *endAd
 extern unsigned int readSequence(unsigned char *beginAddres, uint8_t barLength);
 extern int8_t convertSequence(unsigned int sequence);
 
-void printSequences(unsigned char *beginAddress, unsigned char *endAddress)
+int printSequences(unsigned char *beginAddress, unsigned char *endAddress)
 {
     int stopReached = 0;
     unsigned char *addrAfterQuiet = addressAfterQuiet(beginAddress, endAddress);
     uint8_t narrowestBarLenght = getNarrowestBar(beginAddress, endAddress);
+    int distToNextSeq = 3 * 11 * narrowestBarLenght;
 
     while(!stopReached)
     {
@@ -21,28 +22,76 @@ void printSequences(unsigned char *beginAddress, unsigned char *endAddress)
         unsigned int convertedSequence = convertSequence(sequence);
 
         if(sequence != START_CODE && sequence != STOP_CODE)
-        {
-            printf("Sequence value: %i, decoded value: ", sequence);
+        {  
+            if(convertedSequence == -1)
+                return 0;
 
-            if(convertedSequence < 10) printf("0");
+            unsigned int nextSequence = readSequence(addrAfterQuiet + distToNextSeq, narrowestBarLenght);
 
-            printf("%i\n", convertedSequence);
+            if(nextSequence != STOP_CODE)
+            {
+                if(convertedSequence < 10) printf("0");
+                printf("%i", convertedSequence);
+            }
         }
         else if(sequence == STOP_CODE)
             stopReached = 1;
 
-        addrAfterQuiet += 3 * 11 * narrowestBarLenght;
+        addrAfterQuiet += distToNextSeq;
     }
+
+    return 1;
 }
 
-int main()
+void printHelp()
 {
-    ImageInfo* imgInfo = readBmp("cbarcode.bmp");
-    unsigned char *beginAddress = imgInfo->pImg + (imgInfo->height / 2) * imgInfo->line_bytes;
-    unsigned char *endAddress = beginAddress + imgInfo->width * 3;
+    printf("The following program reads Code128C barcodes saved in 24-bit bitmap images. Maximum image size: 768x64 pixels.\n\n");
+    printf("USAGE:\ncode128reader.exe [path to the image]\n\n");
+    printf("EXIT CODES:\n0 - success, 1 - invalid arguments amount, 2 - invalid file path, 3 - invalid image.");
+}
 
-    printSequences(beginAddress, endAddress);
+int validateArgs(int argc)
+{
+    if(argc < 2)
+    {
+        printf("Insufficient amount of arguments.\n");
+        printHelp();
+        return 0;
+    }
+    else if(argc > 3)
+    {
+        printf("Too many arguments.\n");
+        printHelp();
+        return 0;
+    }
+    else return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if(!validateArgs(argc))
+        return 1;
+
+    int returnCode = 0;
+    ImageInfo* imgInfo = readBmp(argv[1]);
+
+    if(imgInfo == NULL)
+    {
+        printf("Invalid image file.");
+        returnCode = 2;
+    }
+    else
+    {
+        unsigned char *beginAddress = imgInfo->pImg + (imgInfo->height / 2) * imgInfo->line_bytes;
+        unsigned char *endAddress = beginAddress + imgInfo->width * 3;
+
+        if(!printSequences(beginAddress, endAddress))
+        {
+            printf("\nGiven image contains invalid bars sequence. Make sure to load Code128C barcode.");
+            returnCode = 3;
+        }
+    }
 
     freeImage(imgInfo);
-    return 0;
+    return returnCode;
 }
